d08b2aab8c854984b155c7d514d666da2b224416
Zakariae EL ALAOUI EL BELGHITI <zakariaeelalaouielbelghiti@MacBook-Pro-de-Zakariae-2.local>
2018-08-16 10:33:58 +0200

    Refacto => Rendering with alternative approches

--- lib/products.dart
+++ lib/products.dart
@@ -18,16 +18,22 @@ class Products extends StatelessWidget {
     );
   }
 
+  Widget _buildProductList() {
+    Widget productCard = Center(
+      child: Text('No Products founds, please add some'),
+    );
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    }
+    return productCard;
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] build()');
-    return products.length > 0
-        ? ListView.builder(
-            itemBuilder: _buildProductItem,
-            itemCount: products.length,
-          )
-        : Center(
-            child: Text('No Products founds, please add some'),
-          );
+    return _buildProductList();
   }
 }
