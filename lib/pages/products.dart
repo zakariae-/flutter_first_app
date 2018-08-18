6502136b9fc562486ced9814bee4dafb37e03769
Zakariae EL ALAOUI EL BELGHITI <zakariaeelalaouielbelghiti@MacBook-Pro-de-Zakariae-2.local>
2018-08-18 16:37:05 +0200

    Adding the Sidedrawer and Hamburger Icon

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -5,6 +5,20 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Product'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('First App'),
       ),
