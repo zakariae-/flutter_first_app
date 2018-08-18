c0582f512766f8bc850a55ace6ae54498c9ae18c
Zakariae EL ALAOUI EL BELGHITI <zakariaeelalaouielbelghiti@MacBook-Pro-de-Zakariae-2.local>
2018-08-18 18:22:29 +0200

    Adding Tabs

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,34 +1,54 @@
 import 'package:flutter/material.dart';
 import './products.dart';
+import './product_create.dart';
+import './product_list.dart';
 
 class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text('Choose'),
-            ),
-            ListTile(
-              title: Text('All Product'),
-              onTap: () => Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => ProductsPage(),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('All Product'),
+                onTap: () => Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => ProductsPage(),
+                      ),
                     ),
-                  ),
-            ),
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Product Admin'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                text: 'Create Product',
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+                text: 'My Product',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ProductCreatePage(),
+            ProductListPage()
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: Text('Product Admin'),
-      ),
-      body: Center(
-        child: Text('Product Admin'),
       ),
     );
   }
