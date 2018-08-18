2941cbe6af05cba59ab7e639b38addf9554d68fe
Zakariae EL ALAOUI EL BELGHITI <zakariaeelalaouielbelghiti@MacBook-Pro-de-Zakariae-2.local>
2018-08-18 12:47:59 +0200

    Adding reactions upon button pressing

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
@@ -8,27 +9,33 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text(title),
-          ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              color: Theme.of(context).accentColor,
-              child: Text('DELETE'),
-              onPressed: () => Navigator.pop(context, true),
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(title),
             ),
-          ),
-        ],
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('DELETE'),
+                onPressed: () => Navigator.pop(context, true),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
