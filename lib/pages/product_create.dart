785fbc24bd335a4c61d447c0f620fdfea45d96dc
Zakariae EL ALAOUI EL BELGHITI <zakariaeelalaouielbelghiti@MacBook-Pro-de-Zakariae-2.local>
2018-09-08 11:39:45 +0200

    Configuring Text Fields

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePage();
+  }
+}
+
+class _ProductCreatePage extends State<ProductCreatePage> {
+  String titleValue = '';
+  String decriptionValue = '';
+  double priceValue;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(context: context, builder: (BuildContext context) {
-            return Center(child: Text('This is a Modal'),);
-          });
-        },
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Title'),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Description'),
+            maxLines: 4,
+            onChanged: (String value) {
+              setState(() {
+                decriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Price'),
+            keyboardType: TextInputType.number,
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+        ],
       ),
     );
   }
