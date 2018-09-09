c4e3cafb082a844c2762d59b1f998091d732717a
Zakariae EL ALAOUI EL BELGHITI <zakariaeelalaouielbelghiti@MacBook-Pro-de-Zakariae-2.local>
2018-09-09 16:38:27 +0200

    Refacto Code

---
+++ lib/widgets/products/product_card.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import './price_tag.dart';
+
+class ProductCard extends StatelessWidget {
+  final Map<String, dynamic> product;
+  final int productIndex;
+
+  ProductCard(this.product, this.productIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(product['image']),
+          Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  product['title'],
+                  style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Oswald'),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                PriceTag(product['price'].toString()),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey, width: 1.0),
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+            child: Text('Rue Honore d Estienne d Orves, Suresnes'),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.info),
+                onPressed: () => Navigator.pushNamed<bool>(
+                    context, '/product/' + productIndex.toString()),
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite_border),
+                color: Colors.red,
+                onPressed: () {
+                  Navigator.pushNamed<bool>(
+                      context, '/product/' + productIndex.toString());
+                },
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/products/products.dart
+++ lib/widgets/products/products.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './price_tag.dart';
+
+import './product_card.dart';
 
 class Products extends StatelessWidget {
   final List<Map<String, dynamic>> products;
@@ -8,68 +9,14 @@ class Products extends StatelessWidget {
     print('[Products Widget] Constructor');
   }
 
-  Widget _buildProductItem(BuildContext context, int index) {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Image.asset(products[index]['image']),
-          Container(
-            padding: EdgeInsets.only(top: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  products[index]['title'],
-                  style: TextStyle(
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Oswald'),
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                PriceTag(products[index]['price'].toString()),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 1.0),
-              borderRadius: BorderRadius.circular(4.0),
-            ),
-            child: Text('Rue Honore d Estienne d Orves, Suresnes'),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.info),
-                onPressed: () => Navigator.pushNamed<bool>(
-                    context, '/product/' + index.toString()),
-              ),
-              IconButton(
-                icon: Icon(Icons.favorite_border),
-                color: Colors.red,
-                onPressed: () {
-                  Navigator.pushNamed<bool>(
-                      context, '/product/' + index.toString());
-                },
-              )
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
   Widget _buildProductList() {
     Widget productCard = Center(
       child: Text('No Products founds, please add some'),
     );
     if (products.length > 0) {
       productCard = ListView.builder(
-        itemBuilder: _buildProductItem,
+        itemBuilder: (BuildContext context, int index) =>
+            ProductCard(products[index], index),
         itemCount: products.length,
       );
     }
